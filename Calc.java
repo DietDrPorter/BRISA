2a3da6c3aeb82e5a8e7ba5833d6eceba491e9867
Brisa Porter <brisaporter06@gmail.com>
2018-02-18 17:10:35 -0800

    Help

--- Calc.java
+++ Calc.java
@@ -1,1 +1,214 @@
+import java.util.Scanner;
 
+public class LoanCalculator{
+    public static int mode = -1;
+    public static double loanAmt = -1;
+    public static int loanTerm = -1;
+    public static double interestRate = -1;
+    public static double totalLoan = -1;
+    public static double interesttotal = -1;
+   
+
+    public static void main(String[] args) {
+        //Read in the loan calculation mode
+        Scanner scan;
+        while(true) { //Loop the mode selection menu until a valid input is provided
+            scan = new Scanner(System.in);
+            printModeStatement(); //Call a function to print our mode selection statement
+            if(scan.hasNextInt()) { //Check if input provided is a valid integer
+                mode = scan.nextInt();
+                if(0 < mode && mode < 4) { //Check if input provided is 1, 2, or 3
+                    break; //Valid input has been provided. Break out of the while loop
+                }
+            }
+            //If it reaches here, the input was invalid. Loop again.
+        }
+
+   
+
+    
+        if(mode == 1) {
+            flatInterest();
+        }
+        if(mode == 2) {
+            cmpndWOInterest();
+        }
+        if(mode == 3) {
+            cmpndWInterest();
+        }
+        else {
+            System.out.println("Not a valid mode."); //Replace this with calls to your appropriate functions            
+        }
+    }
+
+    public static void flatInterest() {
+
+        Scanner scan;
+        while(true) { 
+            scan = new Scanner(System.in);
+            System.out.println("Enter your loan amount:");
+            if(scan.hasNextDouble()) { 
+                loanAmt = scan.nextDouble();
+                if(0 < loanAmt) { 
+                    break; 
+                }
+            }
+            
+        }
+
+        while(true) { 
+            scan = new Scanner(System.in);
+            System.out.println("Enter your loan term (years):");
+            if(scan.hasNextInt()) { 
+                loanTerm = scan.nextInt();
+                if(0 < loanTerm) {
+
+                    break;
+                }
+            }
+            
+        }
+
+        
+        while(true) { 
+            scan = new Scanner(System.in);
+            System.out.println("Enter your interest rate (%):");
+            if(scan.hasNextDouble()) { 
+                interestRate = scan.nextDouble();
+                if(0 < interestRate) { 
+                    break; 
+                }
+            }
+            
+        }
+
+        double interestPercent = interestRate / 100;
+        double loanMonths = loanTerm * 12;
+        double interesttotal = loanAmt * interestPercent * loanMonths;
+        double totalLoan = loanAmt + interesttotal;
+        
+
+        System.out.println("You selected mode " + mode);
+        System.out.println("Original Loan Amount:" + loanAmt);
+        System.out.println("Loan Term:" + loanTerm);
+        System.out.println("Interest Rate:"+ interestRate);
+        System.out.println("Interest to be Paid:" + interesttotal);
+        System.out.println("Total Loan:" + totalLoan);
+    } 
+
+    
+    public static void cmpndWOInterest() {
+
+        Scanner scan;
+        while(true) { 
+            scan = new Scanner(System.in);
+            System.out.println("Enter your loan amount:");
+            if(scan.hasNextDouble()) { 
+                loanAmt = scan.nextDouble();
+                if(0 < loanAmt) { 
+                    break; 
+                }
+            }
+            
+        }
+
+        while(true) { 
+            scan = new Scanner(System.in);
+            System.out.println("Enter your loan term (years):");
+            if(scan.hasNextInt()) { 
+                loanTerm = scan.nextInt();
+                if(0 < loanTerm) {
+
+                    break;
+                }
+            }
+            
+        }
+
+        
+        while(true) { 
+            scan = new Scanner(System.in);
+            System.out.println("Enter your interest rate (%):");
+            if(scan.hasNextDouble()) { 
+                interestRate = scan.nextDouble();
+                if(0 < interestRate) { 
+                    break; 
+                }
+            }
+            
+        }
+
+        double interestPercent = interestRate / 100;
+        double loanMonths = loanTerm * 12;
+        double loanTotal = loanAmt * Math.pow(1 + (interestPercent / 12), loanMonths);
+        double totalInterest = loanTotal - loanAmt;
+
+
+
+        System.out.println("You selected mode " + mode);
+        System.out.println("Original Loan Amount:" + loanAmt);
+        System.out.println("Loan Term:" + loanTerm);
+        System.out.println("Interest Rate:"+ interestRate);
+        System.out.println("Interest to be Paid:" + interesttotal);
+        System.out.println("Total Loan:" + totalLoan);
+    } 
+
+    public static void cmpndWOInterest() {
+
+        Scanner scan;
+        while(true) { 
+            scan = new Scanner(System.in);
+            System.out.println("Enter your loan amount:");
+            if(scan.hasNextDouble()) { 
+                loanAmt = scan.nextDouble();
+                if(0 < loanAmt) { 
+                    break; 
+                }
+            }
+            
+        }
+
+        while(true) { 
+            scan = new Scanner(System.in);
+            System.out.println("Enter your loan term (years):");
+            if(scan.hasNextInt()) { 
+                loanTerm = scan.nextInt();
+                if(0 < loanTerm) {
+
+                    break;
+                }
+            }
+            
+        }
+
+        
+        while(true) { 
+            scan = new Scanner(System.in);
+            System.out.println("Enter your interest rate (%):");
+            if(scan.hasNextDouble()) { 
+                interestRate = scan.nextDouble();
+                if(0 < interestRate) { 
+                    break; 
+                }
+            }
+            
+        }
+
+
+        double interestPercent = interestRate / 100;
+        double ratePerMonth = interestPercent /12;
+        double monthlyPayment = 1 + loanAmt * (ratePerMonth / (1 - Math.pow((1 + ratePerMonth), (loanTerm * -12))));
+        double totalLoan = loanAmt * monthlyPayment;
+        double interesttotal = totalLoan - loanAmt;
+
+
+
+        System.out.println("You selected mode " + mode);
+        System.out.println("Original Loan Amount:" + loanAmt);
+        System.out.println("Loan Term:" + loanTerm);
+        System.out.println("Monthly Payment:"+ monthlyPayment);
+        System.out.println("Interest to be Paid:" + interesttotal);
+        System.out.println("Total Loan:" + totalLoan);
+    } 
+
+}
