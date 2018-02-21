28a5163d1f85bad725d0fd5f897317db8098b293
Brisa Porter <brisaporter06@gmail.com>
2018-02-20 17:56:40 -0800

    Added project folder

--- HelloWorld/Operators.java
+++ HelloWorld/Operators.java
@@ -26,16 +26,38 @@ class Operators {
 //String scentenceOne = firstName + " " + connect + " " + friendName + " " + were + " " + comeBack + " " + to + " " + what + " " + we  + " " ;
 //System.out.println(scentenceOne);
 
-int loanAmount = 50000;
-double interestRate = .08;
-int loanPaybackPeriod = 12;
-double interestPerMonth = loanAmount * interestRate / loanPaybackPeriod;
-double totalLoan = loanAmount + interestPerMonth;
+//int loanAmount = 50000;
+//double interestRate = .08;
+//int loanPaybackPeriod = 12;
+//double interestPerMonth = loanAmount * interestRate / loanPaybackPeriod;
+//double totalLoan = loanAmount + interestPerMonth;
 
-System.out.println(interestPerMonth);
-System.out.println("Interest Per Month");
-System.out.println(totalLoan);
-System.out.println("Total Loan");
+//System.out.println(interestPerMonth);
+//System.out.println("Interest Per Month");
+//System.out.println(totalLoan);
+//System.out.println("Total Loan");
+        
+            Boolean hasBoyfriend = false;
+        Boolean hasMoney = true;
+        Boolean hasJob = true;
+
+        if(!hasJob) { 
+            if(hasMoney && hasJob) { 
+                System.out.println("You don't need a boyfriend." );
+            }
+            else { 
+                System.out.println("Get you a man.");
+
+            }
+        }
+if(!hasBoyfriend) { 
+    System.out.println("Make sure he has money and job.");
+}
+else {
+    System.out.println("Look at your schedule. Is it worth it?");
+}
 
     }
 }
+
+
