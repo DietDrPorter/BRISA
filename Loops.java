70954fee84cce14ca7acac7798ea1c7862249b7e
Brisa Porter-Garcia <Porte.Bris2018@ssusd.org>
2018-02-22 07:40:02 -0800

    While loops

---
+++ Loops.java
@@ -0,0 +1,34 @@
+
+
+public class Loops{
+
+
+    public static void main(String[] args) {
+     
+
+        //int n = 1;
+        //while(n < 10) {
+        // System.out.println(n);
+            //n++;
+         // }
+
+         //Boolean shouldLoop = true;
+         //   while(shouldLoop) {
+            //if(n == 100) {
+           // shouldLoop = false;
+          //  }
+            //System.out.println("100 loop passes occured.");
+            //n++;
+            // }
+       // int n = 100;
+        //while (n > -101) {
+           // System.out.println(n);
+           // n--;
+           // if (n == -100) {
+               // n--;
+            //}
+            
+         //}   
+
+    }
+}
