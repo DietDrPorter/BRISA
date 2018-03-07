284cadeaece7053a8499402a5e2d135233e033ae
Brisa Porter-Garcia <Porte.Bris2018@ssusd.org>
2018-03-07 08:03:37 -0800

    Adds array assignment

--- Assignments/Array.java
+++ Assignments/Array.java
@@ -1,1 +1,37 @@
 
+class Array {
+
+    public static void main(String args[])
+        {
+        /* int[] numArray = {20, 3, 6, 10, 13, 25, 100, 6, 3, 20, 5102};
+
+            System.out.println( numArray[3] ); //10
+
+            System.out.println( numArray[11] ); //? (just prints 10)
+
+            System.out.println( numArray[1] ); //3
+
+            System.out.println( numArray[7] ); //6 
+
+            System.out.println( numArray[8] ); //3 
+
+        int[] numArray = {20, 3, 6, 10, 13, 25, 100, 6, 3, 20, 5102};
+
+            System.out.println( numArray[4] ); //13
+
+            System.out.println( numArray[1] ); //3
+
+            System.out.println( numArray[10] ); //5102
+
+            System.out.println( numArray[5] ); //25
+
+            System.out.println( numArray[0] ); //20 */
+
+            int[] fibonacci = {1, 1, 2, 3, 0, 8, 13, 0, 34, 0};
+
+            System.out.println( fibonacci[4] = fibonacci[2] + fibonacci[3] ); 
+            System.out.println( fibonacci[7] = fibonacci[5] + fibonacci [6] ); 
+            System.out.println( fibonacci[9]= fibonacci[7] + fibonacci[8] ); 
+            
+        }
+}    
