7d19ad6028475050bbb9c858f8c270ae76972471
Brisa Porter-Garcia <Porte.Bris2018@ssusd.org>
2018-03-08 07:37:23 -0800

    adds first part of scope assignment

--- Assignments/Life.java
+++ Assignments/Life.java
@@ -1,1 +1,41 @@
+ class ScopeTest {
+       /* public static String message = "Hello";
+        public static Boolean shouldLoop = false;
 
+        public static void main(String args[]) {
+            int i = 0;
+            while(i <= 10) {
+                int n = i * 2;
+                System.out.println(n);
+                i++;
+            
+                if(n > 10) {
+                String message = "Hello World";
+                System.out.println(message);
+                 }
+            }     
+
+            shouldLoop = true;
+            
+            while(shouldLoop) {
+                if(i < 10) {
+                    System.out.println(message);                    
+                } else {
+                    Boolean shouldLoop = false;
+                }
+                i++;
+            }
+        } */
+
+        public static void main(String args[]) { 
+            String message = "Hello World";
+                for(int i = 0; i < message.length(); i++) {
+                    if(message.char(i) == 'W') {
+                     break;
+                    }
+                }             
+        
+        }    
+
+
+    }
