6ae57c2254e16c49f3f3449e2ad0d0f43b186f70
Brisa Porter <brisaporter06@gmail.com>
2018-03-10 21:31:17 -0800

    More Scope

--- Assignments/Powers.java
+++ Assignments/Powers.java
@@ -1,1 +1,14 @@
+ class Powers {
+    public static final int n = 32;
+    public static int base = 2;
 
+    public static void main(String[] args) {
+        for(int i = 0; i <= n; i++) {
+            System.out.println(double powers());
+        }
+    }
+
+    public static void powers(double power) {
+        return Math.pow(base, n);
+    }
+}
--- Assignments/ScopeTest.java
+++ Assignments/ScopeTest.java
@@ -42,18 +42,4 @@
 
  } */
 
-    class Powers {
-    public static final int n = 32;
-    public static int base = 2;
-
-    public static void main(String[] args) {
-        int i;
-        for( i = 0; i <= n; i++) {
-            power();
-        }
-    }
-
-    public static void power() {
-        return -1;
-    }
-}
+   
