b5ddc392cf32d118720cd5407e0f82f286c114d6
Brisa Porter-Garcia <Porte.Bris2018@ssusd.org>
2018-03-15 07:39:46 -0700

    .

--- Assignments/Lists.java
+++ Assignments/Lists.java
@@ -1,1 +1,23 @@
+import java.util.*; 
+class Lists {
 
+    public static void main(String[] args) {
+         ArrayList<Integer> numList = new ArrayList<Integer>();
+         
+         System.out.print(list);
+            numList.add(5);
+            numList.add(0,10);
+            numList.add(1,15);
+            numList.remove(1);
+            numList.add(20);
+            numList.add(1,25);
+            numList.add(0, 30);
+            numList.add(3, 35);
+            numList.remove(2);
+            list.size()-1;
+
+    } 
+
+
+
+}
