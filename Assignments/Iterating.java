ca6b600f618540b845b1be341bf9e91d5caa18fb
Brisa Porter <brisaporter06@gmail.com>
2018-03-15 16:44:59 -0800

    Iterating finished

--- Assignments/Iterating.java
+++ Assignments/Iterating.java
@@ -1,1 +1,52 @@
 
+class IteratingData {
+    public static void main(String[]args) {
+
+       // int[] nums = new int[201]; 
+        //nums[0] = -100;
+        /*
+        for(int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i-1] + 1;
+            System.out.println(nums[i]);
+
+        } 
+
+        for(int i = 0; i < nums.length; i++) {
+            if(i < 100) {
+                nums[i] = nums[i] * -1;
+                System.out.println(nums[i]);
+            }
+            else {
+                System.out.println(nums[i]);
+            }
+        } 
+
+        int[] fibonacci = new int [41];
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+        for(int i = 2; i < fibonacci.length; i++) {
+            fibonacci[i] = fibonacci[i-2] + fibonacci[i-1];
+    
+            System.out.println(fibonacci[i]); 
+        } */
+
+       // int[] numbers;
+       // for(int value : numbers) {
+          //  if(value % 2 == 1) {
+               // System.out.println(value);
+           // }
+       // }
+
+
+        String[] messages = new String [];
+       // String[] messages;
+        for(String words : messages){
+            if(words.length() >= 10){
+                System.out.println(words);
+            }
+        }
+        
+            
+
+    }
+}
